3755b679493894861dab272263ab912a80150248
I750248 <dan.miuta.24@lehre.mosbach.dhbw.de>
2025-09-29 01:51:01 +0200

    Add Greatest Common Divisor using Euclidean algorithm

---
+++ algorithms/GCD.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class GCD {
+    public static int gcd(int a, int b) {
+        a = Math.abs(a); b = Math.abs(b);
+        while (b != 0) {
+            int t = a % b;
+            a = b; b = t;
+        }
+        return a;
+    }
+}
